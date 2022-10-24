884b7bf7807d758b401cea6ad1e1bd78054266dd
Lukas <lukasribbestad@hotmail.com>
2022-10-24 10:16:53 +0200

    Etapp 3: Results

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 let darkModeBtn = document.querySelector("#darkModeBtn");
+let darkMode = false;
 
 darkModeBtn.addEventListener("click", () => {
-    document.body.style.background = "grey";
-    document.body.style.color = "white";
+    if(darkMode===true) {
+        document.body.style.background = "white";
+        document.body.style.color = "black";
+        darkMode = false;
+    } else {
+        document.body.style.background = "grey";
+        document.body.style.color = "white";
+        darkMode = true;
+    }
  });
 
 /*Funktioner:
@@ -17,80 +25,80 @@ let questionArr = [
     {
         question: "Pocahontas är baserad på en verklig person.",
         answers: {
-            a: true,
-            b: false
+            a: "sant",
+            b: "falskt"
         },
         correctAnswer: "a"
     },
     {
         question:"Aristocats utspelar sig i London.",
         answers: {
-            a: true,
-            b: false
+            a: "sant",
+            b: "falskt"
         },
         correctAnswer: "b"
     },
     {
         question: "Snövit är disneys första tecknade långfilm.",
         answers: {
-            a: true,
-            b: false
+            a: "sant",
+            b: "falskt"
         },
         correctAnswer: "a"
     },
     {
         question: "Lejonkungen skulle ursprungligen heta djungelkungen.",
         answers: {
-            a: true,
-            b: false
+            a: "sant",
+            b: "falskt"
         },
         correctAnswer: "a"
     },
     {
         question: "Första tanken var att Elsa i Frost skulle vara skurken.",
         answers: {
-            a: true,
-            b: false
+            a: "sant",
+            b: "falskt"
         },
         correctAnswer: "a"
     },
     {
         question: "Hitta Nemo kom ut 2005",
         answers: {
-            a: true,
-            b: false
+            a: "sant",
+            b: "falskt"
         },
         correctAnswer: "b"
     },
     {
         question: "Sulley i Monsters Inc heter igentligen James.",
         answers: {
-            a: true,
-            b: false
+            a: "sant",
+            b: "falskt"
         },
         correctAnswer: "a"
     },
     {
         question: "Pinochio dyker upp i filmen Trassel.",
         answers: {
-            a: true,
-            b: false
+            a: "sant",
+            b: "falskt"
         },
         correctAnswer: "a"
     },
     {
         question: "Dvärgarna i Snövit saknar alla varsit finger.",
         answers: {
-            a: true,
-            b: false
+            a: "sant",
+            b: "falskt"
         },
         correctAnswer: "a"
     },
     {
         question: "Bajen är bäst i stan.",
         answers: {
-            a: true,
-            b: false
+            a: "sant",
+            b: "falskt"
         },
         correctAnswer: "a"
     }
@@ -123,7 +131,30 @@ function createQuiz(questions, quizContent, resultsContent, resultBtn){
         };
         
         function showResults(questions, quizContent, resultsContent) {
+            let answerContent = quizContent.querySelectorAll(".answers");
+            let checkedAnswer = "";
+            let correctAmount = 0;
+
+            for(let i=0; i<questions.length; i++) {
+                checkedAnswer = (answerContent[i].querySelector("input[name=question"+i+"]:checked")||
+                {}).value;
+                if(checkedAnswer===questions[i].correctAnswer) {
+                    correctAmount++;
+                } 
+            };
             
+            if(correctAmount>7) {
+                resultsContent.style.color = "lightgreen";
+                resultsContent.innerHTML = correctAmount + " / " + questions.length + " Mycket väl godkänd!";
+            }
+            else if(correctAmount>=5) {
+                resultsContent.style.color = "orange";
+                resultsContent.innerHTML = correctAmount + " / " + questions.length + " Godkänd!";
+            }
+            else {
+                resultsContent.style.color = "red";
+                resultsContent.innerHTML = correctAmount + " / " + questions.length + " Underkänd.";
+            }
         };
         showQuestions(questions, quizContent);
         
